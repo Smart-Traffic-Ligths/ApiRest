c8c2139282747ef6f0fd5c6bea94b2be7c84e9ba
unknown <yarit.villalobos@mail.escuelaing.edu.co>
2021-12-03 22:12:32 -0500

    arreglando DataDto

--- src/main/java/edu/eci/apirest/controller/dto/DataDto.java
+++ src/main/java/edu/eci/apirest/controller/dto/DataDto.java
@@ -4,78 +4,124 @@ import java.util.Date;
 
 public class DataDto {
 	
-	private long trafficlight;
+	private String traffic_light;
 	private String lights_status;
-	private long duration_ligths;
+	private long duration_lights;
 	private long traffic_volume;
 	private int lane_occupancy;
 	private long speed;
-	private Date time_lights;
+	private String date_day;
+	private String time_day;
 	
-	public DataDto(long trafficlight, String lights_status, long duration_ligths, long traffic_volume, int lane_occupancy, long speed, Date time_lights) {
-		this.trafficlight = trafficlight;
+	
+
+	public DataDto(String traffic_light, String lights_status, long duration_ligths, long traffic_volume,
+			int lane_occupancy, long speed, String date_day, String time_day) {
+		super();
+		this.traffic_light = traffic_light;
 		this.lights_status = lights_status;
-		this.duration_ligths = duration_ligths;
+		this.duration_lights = duration_ligths;
 		this.traffic_volume = traffic_volume;
 		this.lane_occupancy = lane_occupancy;
 		this.speed = speed;
-		this.time_lights = time_lights;
+		this.date_day = date_day;
+		this.time_day = time_day;
 	}
 
-	public long getTrafficlight() {
-		return trafficlight;
+
+
+	public String getTraffic_light() {
+		return traffic_light;
 	}
 
-	public void setTrafficlight(long trafficlight) {
-		this.trafficlight = trafficlight;
+
+
+	public void setTraffic_light(String traffic_light) {
+		this.traffic_light = traffic_light;
 	}
+
+
 
 	public String getLights_status() {
 		return lights_status;
 	}
 
+
+
 	public void setLights_status(String lights_status) {
 		this.lights_status = lights_status;
 	}
 
+
+
 	public long getDuration_ligths() {
-		return duration_ligths;
+		return duration_lights;
 	}
 
+
+
 	public void setDuration_ligths(long duration_ligths) {
-		this.duration_ligths = duration_ligths;
+		this.duration_lights = duration_ligths;
 	}
+
+
 
 	public long getTraffic_volume() {
 		return traffic_volume;
 	}
 
+
+
 	public void setTraffic_volume(long traffic_volume) {
 		this.traffic_volume = traffic_volume;
 	}
+
+
 
 	public int getLane_occupancy() {
 		return lane_occupancy;
 	}
 
+
+
 	public void setLane_occupancy(int lane_occupancy) {
 		this.lane_occupancy = lane_occupancy;
 	}
+
+
 
 	public long getSpeed() {
 		return speed;
 	}
 
+
+
 	public void setSpeed(long speed) {
 		this.speed = speed;
 	}
 
-	public Date getTime_lights() {
-		return time_lights;
+
+
+	public String getDate_day() {
+		return date_day;
 	}
 
-	public void setTime_lights(Date time_lights) {
-		this.time_lights = time_lights;
+
+
+	public void setDate_day(String date_day) {
+		this.date_day = date_day;
+	}
+
+
+
+	public String getTime_day() {
+		return time_day;
+	}
+
+
+
+	public void setTime_day(String time_day) {
+		this.time_day = time_day;
 	}
 	    
 }
--- src/main/java/edu/eci/apirest/service/DataService.java
+++ src/main/java/edu/eci/apirest/service/DataService.java
@@ -1,11 +1,13 @@
 package edu.eci.apirest.service;
 
-import edu.eci.apirest.controller.dto.DataDto;
+import org.springframework.stereotype.Service;
 
+import edu.eci.apirest.controller.dto.DataDto;
+@Service
 public class DataService {
 
 	public Object analyzeData(DataDto dataDto) {
-		// TODO Auto-generated method stub
+		System.out.println("Analizando datos...");
 		return null;
 	}
 
