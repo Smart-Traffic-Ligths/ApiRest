9061de5519fa36ce38942fa547907f9ee54fea05
Angelica Alfaro <maria.alfaro@mail.escuelaing.edu.co>
2021-12-08 17:25:23 -0500

    Implementando conexión con el balanceador

--- src/main/java/edu/eci/apirest/service/DataService.java
+++ src/main/java/edu/eci/apirest/service/DataService.java
@@ -1,19 +1,35 @@
 package edu.eci.apirest.service;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import edu.eci.apirest.controller.dto.DataDto;
+
 @Service
 public class DataService {
+	
+	private RestTemplate restTemplate;
+	
+	@Autowired
+	public DataService(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
 
-	public Object analyzeData(DataDto dataDto) {
+	public void analyzeData(DataDto dataDto) {
 		System.out.println("Analizando datos...");
-		return null;
 	}
 
-	public Object dataLog(DataDto dataDto) {
-		// TODO Auto-generated method stub
-		return null;
+	public void dataLog(DataDto dataDto) {
+		HttpHeaders headers = new HttpHeaders();
+	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	    HttpEntity <DataDto> entity = new HttpEntity<DataDto>(dataDto,headers);
+	    restTemplate.exchange("http://localhost:8082/v1/logs", HttpMethod.POST, entity, Void.class);
 	}
-
 }
